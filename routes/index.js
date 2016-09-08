b15342792b3761975d8c143bacf23ecee12cca44
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-08 17:07:10 +0800

    Fixed comments to use /* */ instead of // Changed spaces indention to tabs.

--- routes/index.js
+++ routes/index.js
@@ -45,27 +45,27 @@ router.get('/newuser', function(req, res) {
 /* POST to Add User Service */
 router.post('/adduser', function(req, res) {
 
-    // Set our internal DB variable
+    /* Set our internal DB variable */
     var db = req.db;
 
-    // Get our form values. These rely on the "name" attributes
+    /* Get our form values. These rely on the "name" attributes */
     var userName = req.body.username;
     var userEmail = req.body.useremail;
 
-    // Set our collection
+    /* Set our collection */
     var collection = db.get('usercollection');
 
-    // Submit to the DB
+    /* Submit to the DB */
     collection.insert({
         "username" : userName,
         "email" : userEmail
     }, function (err, doc) {
         if (err) {
-            // If it failed, return error
+            /* If it failed, return error */
             res.send("There was a problem adding the information to the database.");
         }
         else {
-            // And forward to success page
+            /* And forward to success page */
             res.redirect("userlist");
         }
     });
