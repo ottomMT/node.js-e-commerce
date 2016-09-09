635a52c92de2dce9e6f15275fd9063eff72f7005
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-09 18:08:07 +0800

    Made the add product work, but still needs some improvement. Added query for rendering products on the front products page. This will also need to be improved, need to use pagination here.

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,14 @@ router.get('/register', function(req, res, next) {
 
 /* GET products page. */
 router.get('/products', function(req, res, next) {
-  res.render('front/products', { title: 'Product' });
+	
+	var collection = req.db.get('products');
+	collection.find({},{},function(e,docs){
+		res.render('front/products', {
+			'products' : docs,
+			'title' : 'Products'
+		});
+	});
 });
 
 /* GET User Account page. */
@@ -34,6 +41,45 @@ router.get('/user/products', function(req, res, next) {
 /* GET User Products Add page. */
 router.get('/user/products/add', function(req, res, next) {
   res.render('user/products-add', { title: 'Add Product' });
+});
+
+/* POST to Add User Service */
+router.post('/user/products/create', function(req, res) {
+
+    /* Set our internal DB variable */
+    var db = req.db;
+
+    /* Get our form values. These rely on the "name" attributes */
+    var product_name = req.body.name;
+    var product_content = req.body.content;
+	var product_excerpt = req.body.excerpt;
+    var product_price = req.body.price;
+    var product_status = req.body.status;
+    var product_quantity = req.body.quantity;
+    var product_date = req.body.date;
+  
+    /* Set our collection */
+    var collection = db.get('products');
+
+    /* Submit to the DB */
+    collection.insert({
+        "name" : product_name,
+        "content" : product_content,
+        "excerpt" : product_excerpt,
+        "price" : product_price,
+        "status" : product_status,
+        "quantity" : product_quantity,
+        "date" : product_date
+    }, function (err, doc) {
+        if (err) {
+            /* If it failed, return error */
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            /* And forward to success page */
+            res.redirect("/user/products/edit");
+        }
+    });
 });
 
 /* GET User Products Edit page. */
