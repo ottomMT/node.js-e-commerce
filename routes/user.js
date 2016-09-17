a375cd7724b90bcd563c78b77c92d7fb2ddd0210
CarloFontanos <carl.fontanos@gmail.com>
2016-09-17 21:53:30 +0800

    Organized the routes, moved them to their more approrpaite and logical folders.

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,27 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/login', function(req, res, next) {
-	res.render('front/login', { title: 'Login' });
+	res.render('user/login', { title: 'Login' });
 });
 
 /* GET registration page. */
 router.get('/register', function(req, res, next) {
-	res.render('front/register', { title: 'Register' });
+	res.render('user/register', { title: 'Register' });
+});
+
+/* POST Create User Account. */
+router.get('/register/create', function(req, res, next) {
+	
 });
 
 /* GET User Account page. */
 router.get('/account', function(req, res, next) {
 	res.render('user/account', { title: 'Account' });
+});
+
+/* POST Update User Account Info. */
+router.get('/account/update', function(req, res, next) {
+	
 });
 
 module.exports = router;
