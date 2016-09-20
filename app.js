a6c9fa0294cbb2237b409f5f7ec7c4b2d3f7bd44
Carlo Fontanos <carl.fontanos@gmail.com>
2016-09-20 09:35:53 +0800

    replaced spaces with tabs.

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ app.use('/', routes);
 
 /* catch 404 and forward to error handler */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /* error handlers */
